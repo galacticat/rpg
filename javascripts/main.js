a4f19b2b03621b7fe62126a883f4e6bf2a71aaea
Chris Garcia <cgarcia865@gmail.com>
2015-08-10 21:18:23 -0500

    background test

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+    $(function(){
+        var x = 0;
+        setInterval(function(){
+            x-=1;
+            $('body').css('background-position', x + 'px 0');
+        }, 10);
+    })
